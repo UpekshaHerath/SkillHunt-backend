7b115c28902dfdfcb6318d90c0e7d91ebd20fd96
UpekshaHerath <upekshadilshan000@gmail.com>
2024-11-29 23:35:12 +0530

    feat: set up cors

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 require("express-async-errors");
 const express = require("express");
+const cors = require('cors');
 const { specs, swaggerUi } = require('./swagger');
 const app = express();
 
@@ -12,20 +13,24 @@ const authenticateUser = require("./middleware/authentication");
 // routers
 const authRouter = require("./routes/auth");
 const jobsRouter = require("./routes/jobs");
-const sampleRouter = require("./routes/sample");
 
 // error handler
 const notFoundMiddleware = require("./middleware/not-found");
 const errorHandlerMiddleware = require("./middleware/error-handler");
 
+
 app.use(express.json());
-// extra packages
+
+app.use(cors({
+  origin: 'http://localhost:3000', 
+  methods: ['GET', 'POST', 'PUT', 'DELETE'], 
+  credentials: true 
+}));
 
 // routes
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(specs));
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/jobs", authenticateUser, jobsRouter); // only the jobsRouter will be protected
-app.use("/", sampleRouter);
 
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
