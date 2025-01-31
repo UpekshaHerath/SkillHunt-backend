997b08287ec0350f17e4af4ddf28b0bc6743fe0a
UpekshaHerath <upekshadilshan000@gmail.com>
2025-02-01 01:42:44 +0530

    feat: new job create

--- models/Job.js
+++ models/Job.js
@@ -38,7 +38,7 @@ const JobSchema = new mongoose.Schema(
     },
     jobType: {
       type: String,
-      enum: ["full-time", "part-time", "freelance"],
+      enum: ["full-time", "part-time", "freelance", "contract", "internship", "temporary", "remote"],
       default: "full-time",
     },
     requirements: {
--- routes/publicJobs.js
+++ routes/publicJobs.js
@@ -5,7 +5,7 @@ const { upload } = require("../config/multer.config");
 const { getPublicJobs, imageTest, imageTestMultiple } = require("../controllers/jobs");
 
 router.route("/").get(getPublicJobs);
-router.post("/test", upload.single("file"), imageTest);
-router.post("/test-multiple", upload.array("files", 2), imageTestMultiple);
+// router.post("/test", upload.single("file"), imageTest);
+// router.post("/test-multiple", upload.array("files", 2), imageTestMultiple);
 
 module.exports = router;
